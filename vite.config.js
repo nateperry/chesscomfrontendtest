f13afa2b7248fbe26fb467cd3716e736a40128fd
Nate Perry <thenateperry@gmail.com>
2023-03-15 00:14:10 -0700

    fix issues with asset path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import {defineConfig, } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [vue()],
-    publicPath: process.env.NODE_ENV === "production" ? "/chesscomfrontendtest/" : "/",
-    define: {
-      __APP_ENV__: process.env.NODE_ENV,
-    },
-  };
+    publicPath: "/chesscomfrontendtest/"
+  }
 })
